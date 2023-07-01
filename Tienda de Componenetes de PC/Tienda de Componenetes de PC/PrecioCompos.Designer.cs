cd02f82f311afd681bf9e10eb6e1d62c496ce48b
LuisPanuni <127569238+LuisPanuni@users.noreply.github.com>
2023-07-01 04:08:12 -0400

    Add files via upload
    Finalización del 3er Sprint

--- Tienda de Componenetes de PC/Tienda de Componenetes de PC/PrecioCompos.Designer.cs
+++ Tienda de Componenetes de PC/Tienda de Componenetes de PC/PrecioCompos.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tienda_de_Componenetes_de_PC
 {
-    partial class ReporteComposDefec
+    partial class PrecioCompos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReporteComposDefec));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrecioCompos));
             label1 = new Label();
             pictureBox2 = new PictureBox();
-            label3 = new Label();
-            label4 = new Label();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            dataGridView1 = new DataGridView();
+            dateTimePicker1 = new DateTimePicker();
             label5 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
             textBox4 = new TextBox();
+            label7 = new Label();
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -51,147 +51,146 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Century", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(272, 57);
+            label1.Location = new Point(245, 31);
             label1.Name = "label1";
             label1.Size = new Size(192, 25);
-            label1.TabIndex = 11;
+            label1.TabIndex = 12;
             label1.Text = "TechTonic Oruro";
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(587, 31);
+            pictureBox2.Location = new Point(595, 21);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(100, 89);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 28;
+            pictureBox2.TabIndex = 29;
             pictureBox2.TabStop = false;
             // 
-            // label3
+            // dateTimePicker1
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(168, 115);
-            label3.Name = "label3";
-            label3.Size = new Size(70, 20);
-            label3.TabIndex = 29;
-            label3.Text = "Telefono:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(342, 114);
-            label4.Name = "label4";
-            label4.Size = new Size(58, 20);
-            label4.TabIndex = 30;
-            label4.Text = "Celular:";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(244, 115);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(92, 23);
-            textBox2.TabIndex = 31;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(406, 112);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 32;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(168, 148);
-            label2.Name = "label2";
-            label2.Size = new Size(75, 20);
-            label2.TabIndex = 33;
-            label2.Text = "Direccion:";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(244, 149);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(262, 23);
-            textBox1.TabIndex = 34;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(13, 192);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(703, 154);
-            dataGridView1.TabIndex = 35;
+            dateTimePicker1.Location = new Point(95, 117);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(211, 23);
+            dateTimePicker1.TabIndex = 37;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(13, 358);
+            label5.Location = new Point(14, 117);
             label5.Name = "label5";
-            label5.Size = new Size(94, 20);
+            label5.Size = new Size(50, 20);
             label5.TabIndex = 36;
-            label5.Text = "Observacion:";
+            label5.Text = "Fecha:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(457, 76);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 35;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(304, 76);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(92, 23);
+            textBox2.TabIndex = 34;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(95, 76);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 23);
+            textBox1.TabIndex = 33;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(402, 77);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 32;
+            label4.Text = "Celular:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(228, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 31;
+            label3.Text = "Telefono:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(14, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 30;
+            label2.Text = "Direccion:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(14, 165);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(703, 197);
+            dataGridView1.TabIndex = 38;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(13, 381);
+            textBox4.Location = new Point(141, 386);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(703, 23);
-            textBox4.TabIndex = 37;
+            textBox4.Size = new Size(157, 23);
+            textBox4.TabIndex = 40;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(19, 385);
+            label7.Name = "label7";
+            label7.Size = new Size(116, 20);
+            label7.TabIndex = 39;
+            label7.Text = "Total Ganancias:";
             // 
             // button1
             // 
-            button1.Location = new Point(325, 436);
+            button1.Location = new Point(627, 429);
             button1.Name = "button1";
-            button1.Size = new Size(93, 23);
-            button1.TabIndex = 38;
-            button1.Text = "Devolver";
+            button1.Size = new Size(90, 29);
+            button1.TabIndex = 41;
+            button1.Text = "Pedir";
             button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            button2.Location = new Point(496, 486);
-            button2.Name = "button2";
-            button2.Size = new Size(93, 23);
-            button2.TabIndex = 39;
-            button2.Text = "Nuevo";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(610, 486);
-            button3.Name = "button3";
-            button3.Size = new Size(93, 23);
-            button3.TabIndex = 40;
-            button3.Text = "Cancelar";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // ReporteComposDefec
+            // PrecioCompos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(728, 521);
-            Controls.Add(button3);
-            Controls.Add(button2);
+            ClientSize = new Size(729, 470);
             Controls.Add(button1);
             Controls.Add(textBox4);
-            Controls.Add(label5);
+            Controls.Add(label7);
             Controls.Add(dataGridView1);
-            Controls.Add(textBox1);
-            Controls.Add(label2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label5);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label4);
             Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(pictureBox2);
             Controls.Add(label1);
-            Name = "ReporteComposDefec";
-            Text = "ReporteComposDefec";
+            Name = "PrecioCompos";
+            Text = "PrecioCompos";
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
@@ -202,17 +201,17 @@
 
         private Label label1;
         private PictureBox pictureBox2;
-        private Label label3;
-        private Label label4;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private Label label2;
-        private TextBox textBox1;
-        private DataGridView dataGridView1;
+        private DateTimePicker dateTimePicker1;
         private Label label5;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private DataGridView dataGridView1;
         private TextBox textBox4;
+        private Label label7;
         private Button button1;
-        private Button button2;
-        private Button button3;
     }
 }
---
+++ Tienda de Componenetes de PC/Tienda de Componenetes de PC/Recibo.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Tienda_de_Componenetes_de_PC
+{
+    partial class Recibo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Recibo));
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            label12 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 25);
+            label1.TabIndex = 13;
+            label1.Text = "TechTonic Oruro";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(606, 31);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 89);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 30;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(276, 38);
+            label2.TabIndex = 31;
+            label2.Text = "Recibo de Venta";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(100, 157);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(211, 23);
+            dateTimePicker1.TabIndex = 45;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(19, 157);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 44;
+            label5.Text = "Fecha:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(253, 251);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 43;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(100, 251);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(92, 23);
+            textBox2.TabIndex = 42;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(100, 221);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(365, 23);
+            textBox1.TabIndex = 41;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(198, 252);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 40;
+            label4.Text = "Celular:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(24, 252);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 39;
+            label3.Text = "Telefono:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(19, 221);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 20);
+            label6.TabIndex = 38;
+            label6.Text = "Direccion:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(19, 192);
+            label7.Name = "label7";
+            label7.Size = new Size(185, 20);
+            label7.TabIndex = 46;
+            label7.Text = "NOMBRE DEL VENDEDOR:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(210, 193);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(255, 23);
+            textBox4.TabIndex = 47;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(223, 280);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(242, 23);
+            textBox5.TabIndex = 48;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(19, 283);
+            label8.Name = "label8";
+            label8.Size = new Size(198, 20);
+            label8.TabIndex = 49;
+            label8.Text = "NOMBRE DEL COMPRADOR:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(24, 318);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(636, 123);
+            dataGridView1.TabIndex = 50;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(75, 461);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(157, 23);
+            textBox6.TabIndex = 52;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(24, 461);
+            label9.Name = "label9";
+            label9.Size = new Size(45, 20);
+            label9.TabIndex = 51;
+            label9.Text = "Total:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(147, 524);
+            label10.Name = "label10";
+            label10.Size = new Size(139, 20);
+            label10.TabIndex = 53;
+            label10.Text = "Firma del Vendedor";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(388, 524);
+            label12.Name = "label12";
+            label12.Size = new Size(151, 20);
+            label12.TabIndex = 55;
+            label12.Text = "Firma del Comprador";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(350, 561);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 22);
+            button1.TabIndex = 56;
+            button1.Text = "IMPRIMIR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(471, 561);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 22);
+            button2.TabIndex = 57;
+            button2.Text = "MODIFICAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(603, 561);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 22);
+            button3.TabIndex = 58;
+            button3.Text = "CANCELAR";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Recibo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(730, 586);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label12);
+            Controls.Add(label10);
+            Controls.Add(textBox6);
+            Controls.Add(label9);
+            Controls.Add(dataGridView1);
+            Controls.Add(label8);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Name = "Recibo";
+            Text = "Recibo";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label8;
+        private DataGridView dataGridView1;
+        private TextBox textBox6;
+        private Label label9;
+        private Label label10;
+        private Label label12;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
